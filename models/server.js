839413fee6da908528e0335bbe468d451b460028
jesus <jesus.david.fernandez.fuentes@gmail.com>
2021-02-23 23:23:52 -0500

    agregando puerto

--- models/server.js
+++ models/server.js
@@ -13,7 +13,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.port=8080;
+        this.port=process.env.PORT;
         this.usuariosPath ='/api/usuarios'; 
 
         //middlewares
